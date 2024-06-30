90169c8993667c6f9df84f2cf78e4cda10e67b01
Oluwatobi02 <olola73@morgan.edu>
2024-06-29 22:34:04 -0400

    integrated the sign up functionality to the sign up button

--- Client/src/main.tsx
+++ Client/src/main.tsx
@@ -7,8 +7,8 @@ import { Auth0Provider } from '@auth0/auth0-react'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
   <Auth0Provider
-    domain=""
-    clientId=""
+    domain="dev-ik11uujq7rk5h5bc.us.auth0.com"
+    clientId="uimcLHky1MEV4mfQoA40A2kB3Tq40Xf7"
     authorizationParams={{
       redirect_uri: 'http://localhost:5173/profile'
     }}
